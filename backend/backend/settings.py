d9170964a43f22ccd14694a79a620034df8c99f7
Александр Хлебнов <khlebnov13@yandex.ru>
2024-11-15 19:40:24 +0300

    Add first version templates

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,6 +100,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/passwords/views.py
+++ backend/passwords/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "passwords/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Передаём мок-данные в шаблон
+        context['resources'] = [
+            {'name': 'GitHub', 'category': 'Development'},
+            {'name': 'Google', 'category': 'Search'},
+        ]
+        return context
